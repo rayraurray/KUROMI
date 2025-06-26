4f90931d28cbdddf28f2ddbb9a9201cd95c7d8f7
Ray <112932654+rayraurray@users.noreply.github.com>
2025-06-27 00:41:34 +0700

    Super Duper Small Fix

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash
 from components.layout import layout
 from components.callbacks import register_callbacks
 
-app = dash.Dash(__name__, assets_folder="assets")
+app = dash.Dash(__name__, assets_folder="assets", suppress_callback_exceptions=True,)
 app.layout = layout
 register_callbacks(app)
 
